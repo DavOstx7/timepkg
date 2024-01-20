b5efad16bbe4e3cca58b3f1537e3a76efbdd9c62
dav ost <private>
2024-01-20 21:57:38 +0200

    rename valid_execution_time to valid_time_margin (more generic)

--- timepkg/validation.py
+++ timepkg/validation.py
@@ -1,2 +1,2 @@
-def valid_execution_time(expected_time: float, actual_time: float, error_margin: float) -> bool:
+def valid_time_margin(expected_time: float, actual_time: float, error_margin: float) -> bool:
     return actual_time - error_margin <= expected_time <= actual_time + error_margin
