baf140dfab189b1e036202ed8be646704ae5d858
dav ost <private>
2024-01-20 20:52:30 +0200

    refactor all fixtures to return a factory lambda

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import random
-from typing import Any
+from typing import Callable, Any
 
 
 MIN_TESTABLE_DURATION = 0
@@ -9,15 +9,15 @@ VALID_ERROR_MARGIN_TIME = 0.1
 
 
 @pytest.fixture
-def random_value() -> Any:
-    return random.choice(["123", "-123", 123, -123, 123.0, -123.0, True, False])
+def value_factory() -> Callable[..., Any]:
+    return lambda: random.choice(["123", "-123", 123, -123, 123.0, -123.0, True, False])
 
 
 @pytest.fixture
-def random_positive_float() -> float:
-    return random.uniform(1, 100)
+def positive_float_factory() -> Callable[..., float]:
+    return lambda: random.uniform(1, 100)
 
 
 @pytest.fixture
-def random_testable_duration() -> float:
-    return random.uniform(MIN_TESTABLE_DURATION, MAX_TESTABLE_DURATION)
+def testable_duration_factory() -> Callable[..., float]:
+    return lambda: random.uniform(MIN_TESTABLE_DURATION, MAX_TESTABLE_DURATION)
