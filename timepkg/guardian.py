2fb2253a2b4254d9aaa95b04aeed1483a25a14c0
dav ost <private>
2024-01-20 18:47:04 +0200

    implement base logic of guardian

--- timepkg/guardian.py
+++ timepkg/guardian.py
@@ -1,7 +1,29 @@
+import time
+from functools import wraps
 from dataclasses import dataclass
-from timepkg.keeper import KeeperResult
+from typing import Callable
+from timepkg.keeper import KeeperResult, Function, Parameters
 
 
 @dataclass
 class GuardianResult(KeeperResult):
     pass
+
+
+GuardianWrapper = Callable[Parameters, GuardianResult]
+GuardianDecorator = Callable[[Function], GuardianWrapper]
+
+
+def guardian() -> GuardianDecorator:
+    def decorator(function: Function) -> GuardianWrapper:
+        @wraps(function)
+        def wrapper(*args: Parameters.args, **kwargs: Parameters.kwargs) -> GuardianResult:
+            start_time = time.perf_counter()
+            return_value = function(*args, **kwargs)
+            end_time = time.perf_counter()
+            execution_time = end_time - start_time
+            return GuardianResult(return_value=return_value, execution_time=execution_time)
+
+        return wrapper
+
+    return decorator
