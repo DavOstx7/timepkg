f4fe10861d9fffd9db4810fc0fdaabd57e3d048f
dav ost <private>
2024-01-20 14:11:03 +0200

    implement timekeeper decorator

--- timepkg/keeper.py
+++ timepkg/keeper.py
@@ -1,6 +1,14 @@
+import time
+from functools import wraps
 from dataclasses import dataclass
-from typing import TypeVar
+from typing import TypeVar, Callable
 
+try:
+    from typing import ParamSpec
+except ImportError:
+    from typing_extensions import ParamSpec
+
+Parameters = ParamSpec("Parameters")
 ReturnType = TypeVar("ReturnType")
 
 
@@ -8,3 +16,19 @@ ReturnType = TypeVar("ReturnType")
 class KeeperResult:
     return_value: ReturnType
     execution_time: float
+
+
+Function = TypeVar("Function", bound=Callable[Parameters, ReturnType])
+KeeperFunction = TypeVar("KeeperFunction", bound=Callable[Parameters, KeeperResult])
+
+
+def timekeeper(function: Function) -> KeeperFunction:
+    @wraps(function)
+    def wrapper(*args: Parameters.args, **kwargs: Parameters.kwargs) -> KeeperResult:
+        start_time = time.perf_counter()
+        return_value = function(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        return KeeperResult(return_value=return_value, execution_time=execution_time)
+
+    return wrapper
