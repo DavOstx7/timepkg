6fc1292bd824ae585c91f834f41599c936a5b9a0
dav ost <private>
2024-01-20 19:04:35 +0200

    add support for catching exceptions in guardian

--- timepkg/guardian.py
+++ timepkg/guardian.py
@@ -1,28 +1,40 @@
 import time
 from functools import wraps
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Optional
 from timepkg.keeper import KeeperResult, Function, Parameters
 
 
 @dataclass
 class GuardianResult(KeeperResult):
-    pass
+    raised_exception: Optional[Exception]
 
 
 GuardianWrapper = Callable[Parameters, GuardianResult]
 GuardianDecorator = Callable[[Function], GuardianWrapper]
 
 
-def guardian() -> GuardianDecorator:
+def guardian(catch_exceptions: bool = False) -> GuardianDecorator:
     def decorator(function: Function) -> GuardianWrapper:
         @wraps(function)
         def wrapper(*args: Parameters.args, **kwargs: Parameters.kwargs) -> GuardianResult:
             start_time = time.perf_counter()
-            return_value = function(*args, **kwargs)
+            return_value = None
+            raised_exception = None
+
+            if catch_exceptions:
+                try:
+                    return_value = function(*args, **kwargs)
+                except Exception as exc:
+                    raised_exception = exc
+            else:
+                return_value = function(*args, **kwargs)
+
             end_time = time.perf_counter()
             execution_time = end_time - start_time
-            return GuardianResult(return_value=return_value, execution_time=execution_time)
+            return GuardianResult(
+                return_value=return_value, execution_time=execution_time, raised_exception=raised_exception
+            )
 
         return wrapper
 
